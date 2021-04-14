256c476195fc17ee119ea907ebdbf6079f42039d
luisglezbrown <lfernanglez@gmail.com>
2021-04-14 10:30:04 +0200

    Corrijo algunos detalles en ejercicio 5 de JS

--- T4_JavaScript/Ejercicios/Ejercicio_5/js/main.js
+++ T4_JavaScript/Ejercicios/Ejercicio_5/js/main.js
@@ -10,19 +10,19 @@ function Book(title, author, sales, price) {
 
 // Variables
 let books = [
-    new Book('Ready Player One', 'Ernest Cline', 987452, 18.95),
-    new Book('Tomás Nevinson', 'Javier Marías', 123456, 21.75),
-    new Book('Transbordo en Moscú', 'Eduardo Mendoza', 152145, 19.85),
-    new Book('Independencia', 'Javier Cercas', 452785, 20.80),
-    new Book('La Anomalía', 'Herve Le Tellier', 984145, 19.45),
-    new Book('Feria', 'Ana Iris Simón', 462286, 21.75),
-    new Book('El Juego del Alma', 'Javier Castillo', 986332, 14.15),
-    new Book('Reina Roja', 'Juan Gómez Jurado', 485174, 22.95),
-    new Book('Rey Blanco', 'Juan Gómez Jurado', 485174, 22.95),
-    new Book('Loba Negra', 'Juan Gómez Jurado', 485174, 22.95)
+    new Book('Ready Player One', 'Ernest Cline', 987452, 18),
+    new Book('Tomás Nevinson', 'Javier Marías', 123456, 21),
+    new Book('Transbordo en Moscú', 'Eduardo Mendoza', 152145, 19),
+    new Book('Independencia', 'Javier Cercas', 452785, 20),
+    new Book('La Anomalía', 'Herve Le Tellier', 984145, 19),
+    new Book('Feria', 'Ana Iris Simón', 462286, 21),
+    new Book('El Juego del Alma', 'Javier Castillo', 986332, 14),
+    new Book('Reina Roja', 'Juan Gómez Jurado', 485174, 22),
+    new Book('Rey Blanco', 'Juan Gómez Jurado', 485174, 22),
+    new Book('Loba Negra', 'Juan Gómez Jurado', 485174, 22)
 ];
+let booksToDisplay = books;
 let cheapestFirst = true;
-
 
 // Asigno selectores
 const table = document.getElementById('tbody');
@@ -42,9 +42,7 @@ const totalPriceCell = document.getElementById('totalPrice');
 function fillList() {
     tbody.innerHTML = '';
 
-    booksToDisplay = books;
     booksToDisplay = books.filter(book => book.title.toLowerCase().includes(inputFilter.value.toLowerCase()))
-
 
     for (let index = 0; index < books.length; index++) {
         books[index].id = index+1;
