38157507692d3e90a1f68269efe1171b13b0848f
luisglezbrown <lfernanglez@gmail.com>
2021-04-15 14:01:43 +0200

    Vemos petición con método HTTP POST

--- T4_JavaScript/Cheatsheet/js/fetch.js
+++ T4_JavaScript/Cheatsheet/js/fetch.js
@@ -38,3 +38,23 @@ async function requestURL(url) {
 console.log(1);
 requestURL(URL);
 console.log(2);
+
+// Petición con método HTTP POST en lugar de GET
+const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const newPost = {
+    title: 'mi título',
+    body: 'mi cuerpo de publicación'
+  }
+
+const http = {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json' // La clave se pasa como string para que acepte el guión
+    },
+    body: JSON.stringify(newPost),    
+};
+
+fetch(POST_URL, http)
+    .then(response => response.json())
+    .then(data => console.log(data))
