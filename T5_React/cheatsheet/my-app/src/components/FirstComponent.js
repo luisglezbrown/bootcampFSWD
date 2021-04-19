5bb69c1055cfabe9caefbb2f7a08172810276f1d
luisglezbrown <lfernanglez@gmail.com>
2021-04-19 13:56:14 +0200

    Comenzamos la introducción a react

---
+++ T5_React/cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,10 @@
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Título</h3>
+            <small>16 de agosto de 2025</small>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique amet nam aut laudantium ad reiciendis?</p>
+        </div>
+    )
+}
+
