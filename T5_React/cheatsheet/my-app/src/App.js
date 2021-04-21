e73dd26f7f3844e0e5bf7dde214d1e94461651b7
luisglezbrown <lfernanglez@gmail.com>
2021-04-21 14:06:54 +0200

    Vemos useState

--- T5_React/cheatsheet/my-app/src/App.js
+++ T5_React/cheatsheet/my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import FirstComponent, {SecondComponent, ThirdComponent} from './components/FirstComponent';
+import Counter from './components/Counter'
 // Los default se importan sin llaves, mientras que el resto lleva llaves.
 
 function App() {
   return (
     <div className="App">
+      <Counter />
       <FirstComponent title='Título enviado desde el padre' date='20 de abril de 2021'/>
       <FirstComponent title='Título'/>
       <SecondComponent />
---
+++ T5_React/cheatsheet/my-app/src/components/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+export default function Counter() {
+
+    // Aquí hacemos una deconstrucción del array con dos
+    // valores que nos devuelve useState.
+    const [count, setCount] = useState(0);
+
+    const handleClick = function (evento) {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <p>Has hecho click {count} veces.</p>
+            <button onClick={handleClick}>Hazme click y sumo!</button>
+            <button onClick={() => setCount(count-1)}>Hazme click y resto!</button>
+            <button onClick={() => setCount(0)}>Te llevo a 0</button>
+        </div>
+    )
+}
