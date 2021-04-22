e4d542c639357af054832f0c6ab2632148a86023
luisglezbrown <lfernanglez@gmail.com>
2021-04-22 14:01:56 +0200

    Corregimos ejercicio 3 a falta del apartado 9

--- T5_React/ejercicios/ejercicio_3/src/App.js
+++ T5_React/ejercicios/ejercicio_3/src/App.js
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import Agenda from './components/Agenda'
 import Formulario from './components/Formulario'
 
-let contactos = [
-  {nombre: 'David', apellidos: 'García Merino', direccion: 'Alcalá, 236', ciudad: 'Madrid', cp: 28028, telefono: 600696969},
-  {nombre: 'Jesús', apellidos: 'Contreras Raboso', direccion: 'Virgen del Coro, 16', ciudad: 'Madrid', cp: 28028, telefono: 654321987},
-  {nombre: 'Carlos', apellidos: 'Llorente Alonso', direccion: 'Agustín Durán, 28', ciudad: 'Madrid', cp: 28028, telefono: 698745321},
-]
-
 function App() {
+
+  const initialContactsState = [
+    {nombre: 'David', apellidos: 'García Merino', direccion: 'Alcalá, 236', ciudad: 'Madrid', cp: 28028, telefono: 600696969},
+    {nombre: 'Jesús', apellidos: 'Contreras Raboso', direccion: 'Virgen del Coro, 16', ciudad: 'Madrid', cp: 28028, telefono: 654321987},
+    {nombre: 'Carlos', apellidos: 'Llorente Alonso', direccion: 'Agustín Durán, 28', ciudad: 'Madrid', cp: 28028, telefono: 698745321},
+  ];
+
+  const [contactos, setContactos] = useState(initialContactsState);
+
+
+ 
   return (
     <div className="App">
       <Agenda contactos={contactos}/>
-      <Formulario />
+      <Formulario setContactos={setContactos}/> 
+      {/* Para añadir el nuevo contacto debo hacerlo con setContactos, por eso lo paso como props */}
     </div>
   );
 }
--- T5_React/ejercicios/ejercicio_3/src/components/Agenda.js
+++ T5_React/ejercicios/ejercicio_3/src/components/Agenda.js
@@ -5,18 +5,18 @@ export default function Agenda({contactos}) {
         
         <div className="container mt-3">
             <h2 className='my-4'>Lista de Contactos</h2>
-            <div className='row row-cols-1 row-cols-md-3'>
+            <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3'>
             
                 {contactos.map(({nombre, apellidos, direccion, ciudad, cp, telefono}, index) => {
                     return   (
-                    <ul class="list-group col mb-4">
-                        <li class="list-group-item active h5" aria-current="true">Contacto {index}</li>
-                        <li class="list-group-item">{nombre}</li>
-                        <li class="list-group-item">{apellidos}</li>
-                        <li class="list-group-item">{telefono}</li>
-                        <li class="list-group-item">{direccion}, {cp}, {ciudad}</li>
-                        <li class="list-group-item">
-                            <button type="button" class="btn btn-warning">Eliminar</button>
+                    <ul className="list-group col mb-4" key={telefono}>
+                        <li className="list-group-item active font-weight-bold" aria-current="true">Contacto {index}</li>
+                        <li className="list-group-item">{nombre}</li>
+                        <li className="list-group-item">{apellidos}</li>
+                        <li className="list-group-item">{telefono}</li>
+                        <li className="list-group-item">{direccion}, {cp}, {ciudad}</li>
+                        <li className="list-group-item">
+                            <button type="button" className="btn btn-warning">Eliminar</button>
                         </li>
                     </ul>)
                     })
