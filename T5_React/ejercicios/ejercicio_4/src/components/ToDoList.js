3803b5091fa21cc92ff9403f4a350c5277dd2c98
luisglezbrown <lfernanglez@gmail.com>
2021-04-26 10:30:40 +0200

    Corregimos el toggle del estado en el ejercicio 4

--- T5_React/ejercicios/ejercicio_4/src/components/ToDoList.js
+++ T5_React/ejercicios/ejercicio_4/src/components/ToDoList.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function ToDoList({toDoList, setToDoList}) {
 
-    const completedToggle = evento => {
-        evento.target.classList.toggle('list-group-item-secondary');
+    const completedToggle = (evento, index) => {
+        const newToDoList = [...toDoList];
+        newToDoList[index].completed = !newToDoList[index].completed;
+
+        setToDoList(newToDoList);
     };
 
     const removeToDo = title => {
@@ -16,7 +19,7 @@ export default function ToDoList({toDoList, setToDoList}) {
             <ul class="list-group mb-5">
                 {toDoList.map((toDo, index) => 
                     // eslint-disable-next-line no-undef
-                    <li className={toDo.completed? 'list-group-item list-group-item-secondary' : "list-group-item"} key={index} onClick={completedToggle}>
+                    <li className={toDo.completed? 'list-group-item list-group-item-secondary' : "list-group-item"} key={index} onClick={evento => completedToggle(evento, index)}>
                         {index+1} - {toDo.title}
                         <button className='btn btn-danger float-right' onClick={removeToDo(toDo.title)}>X</button>
                     </li>                                  
