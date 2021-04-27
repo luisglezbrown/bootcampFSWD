43509f9403eb42294ff74a2970ea4bff28b5ce3d
luisglezbrown <lfernanglez@gmail.com>
2021-04-27 21:48:14 +0200

    Hago ejercicio 5 y comienzo el 6 de react

--- T5_React/ejercicios/ejercicio_5/src/Router.js
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
-
-export default function Router() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
---
+++ T5_React/ejercicios/ejercicio_6/src/components/NewToDoInput.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+export default function NewToDoInput({toDoList, setToDoList}) {
+
+  const [title, setTitle] = useState('');
+
+  const handleTitle = evento => setTitle(evento.target.value);
+
+  function submit (evento) {
+      evento.preventDefault();
+
+      const newTask = {
+        title: title, 
+        completed: false
+      }
+
+      setToDoList([newTask, ...toDoList]);
+
+      setTitle('');
+  }
+
+  return (
+        <form onSubmit={submit}>
+          <input  className='form-control my-4' 
+                  type='text' 
+                  onChange={handleTitle} 
+                  placeholder='Introduce un nuevo To-do'
+                  value ={title}>
+          </input>
+        </form>
+    )
+}
