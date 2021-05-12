9a2b0d50179bc43d37c4c6596653bff4611aa290
luisglezbrown <lfernanglez@gmail.com>
2021-05-12 13:26:16 +0200

    Comenzamos Node

---
+++ T7_Node/cheatsheet/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+//Antes de los endoints, usamos los middlewares
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "Petición GET recibido correctamente"});
+});
+
+app.post("/", (req, res) => {
+    let body = req.body;
+    console.log(body.username + ' vive en la calle ' + body.address.street);
+    
+    if(body.username) {
+        res.status(200).json(
+            {
+                message: `Username recibido: ${body.username}`
+            });
+    } else {
+        res.status(400).json(
+            {
+                ok: false,
+                message: "El username es obligatorio"
+            });
+    }
+
+
+});
+
+app.listen(3000);
